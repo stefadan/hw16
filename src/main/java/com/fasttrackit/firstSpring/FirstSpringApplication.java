05f397e4f110a43e56725860fa6bd0e4df6af5f8
Daniela <stoicadana@hotmail.com>
2022-11-19 13:43:25 +0200

    hw16

--- src/main/java/com/fasttrackit/firstSpring/FirstSpringApplication.java
+++ src/main/java/com/fasttrackit/firstSpring/FirstSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstSpringApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println("FirstSpringApplication");
 		SpringApplication.run(FirstSpringApplication.class, args);
 	}
 
